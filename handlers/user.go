f88efc6eabf6e3351d1a020c02ba32af9a7ba2f2
Aafak Mohammad <aafak-mohammad@hpe.com>
2024-08-12 16:22:52 +0530

    fixed linting

--- handlers/user.go
+++ handlers/user.go
@@ -7,10 +7,12 @@ import (
 	userpb "github.com/aafak/gogrpc/protos/user"
 )
 
+// UserServicer is a struct that implements the UserServiceServer interface
 type UserServicer struct {
 	userpb.UnimplementedUserServiceServer
 }
 
+// CreateUser is a function that implements the CreateUser method of the UserServiceServer interface
 func (s *UserServicer) CreateUser(ctx context.Context, req *userpb.UserRequest) (*userpb.UserResponse, error) {
 	log.Printf("CreateUser function was invoked with request: %+v", req)
 	return &userpb.UserResponse{Name: "Hello " + req.GetName(), Id: "123"}, nil
--- handlers/virtualmachine.go
+++ handlers/virtualmachine.go
@@ -7,10 +7,12 @@ import (
 	vmpb "github.com/aafak/gogrpc/protos/virtualmachine"
 )
 
+// VirtualMachineServicer is a struct that implements the VirtualMachineServiceServer interface
 type VirtualMachineServicer struct {
 	vmpb.UnimplementedVirtualMachineServiceServer
 }
 
+// CreateVirtualMachine is a function that implements the CreateVirtualMachine method of the VirtualMachineServiceServer interface
 func (s *VirtualMachineServicer) CreateVirtualMachine(ctx context.Context, req *vmpb.VirtualMachineRequest) (*vmpb.VirtualMachineResponse, error) {
 	log.Printf("CreateVM function was invoked with request: %+v", req)
 	return &vmpb.VirtualMachineResponse{Name: "Hello " + req.GetName(), Id: "123"}, nil
