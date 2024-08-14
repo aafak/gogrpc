8d7f1c97ee7a327b135c5e5f843ea39db81e8450
Aafak Mohammad <aafak-mohammad@hpe.com>
2024-08-14 15:05:33 +0530

    Fixed linting

--- grpcserver/grpc_server.go
+++ grpcserver/grpc_server.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051") //nolint:gosec
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
